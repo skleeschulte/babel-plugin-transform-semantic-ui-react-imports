81499c7731a2bde013db894893d6953b9b46fb94
Stefan Kleeschulte <stefan.kleeschulte@smk.biz>
2017-06-19 12:38:56 +0200

    [test] test that the same .less file is not imported multiple times

---
+++ test/fixtures/no-double-less-imports/expected.js
@@ -0,0 +1,6 @@
+import Loader from 'semantic-ui-react/dist/es/elements/Loader';
+import 'semantic-ui-less/definitions/elements/loader.less';
+import Rail from 'semantic-ui-react/dist/es/elements/Rail/Rail.js';
+import 'semantic-ui-less/definitions/elements/rail.less';
+import Loader2 from 'semantic-ui-react/dist/es/elements/Loader';
+import Rail2 from 'semantic-ui-react/dist/es/elements/Rail/Rail.js';
---
+++ test/fixtures/no-double-less-imports/source.js
@@ -0,0 +1,4 @@
+import Loader from 'semantic-ui-react/dist/es/elements/Loader';
+import Rail from 'semantic-ui-react/dist/es/elements/Rail/Rail.js';
+import Loader2 from 'semantic-ui-react/dist/es/elements/Loader';
+import Rail2 from 'semantic-ui-react/dist/es/elements/Rail/Rail.js';
